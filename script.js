9f63abc35cc22ed052b24bbc7aee5fa105e2226d
Ashley <aforrest56@gmail.com>
2022-07-31 20:38:51 -0500

    edit

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ let form = document.querySelector('form');
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
        let planet = pickPlanet(listedPlanets);
-       addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.imageURL);
+       addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
     });
    
    form.addEventListener("submit", function(event){
